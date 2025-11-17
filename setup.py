989516e03ad4102324f7ba61f71ce81d016f0546
Krish <krishhrd9@gmail.com>
2025-11-17 21:26:18 +0530

    added code in requirements and setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    versio="0.1.0",
+    author="Bandaru Krishna",
+    author_email="bandaruk62@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
